79774b5d548a1366c6727e6e2b61acced100c7a8
mr-hanwy <hanwy@vip.qq.com>
2022-05-11 22:48:51 +0800

    1.设置TabBarItem选中状态颜色；2.封装NavBar组件。

---
+++ src/components/commons/navBar/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="nav-bar">
+    <div class="nav-bar-left">
+      <slot name="nav-bar-left"/>
+    </div>
+    <div class="nav-bar-center">
+      <slot name="nav-bar-center"/>
+    </div>
+    <div class="nav-bar-right">
+      <slot name="nav-bar-right"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+.nav-bar {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+}
+
+.nav-bar .nav-bar-left,
+.nav-bar .nav-bar-right {
+  width: 60px;
+}
+
+.nav-bar .nav-bar-center {
+  flex: 1;
+}
+</style>
--- src/components/content/mainTabBar/MainTabBar.vue
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <tab-bar>
-      <tab-bar-item path="/home">
+      <tab-bar-item path="/home" activeColor="#ff5777">
         <img slot="item-img" src="~assets/img/tabbar/home.svg" alt=""/>
         <img slot="item-img-active" src="~assets/img/tabbar/home_active.svg" alt=""/>
         <span slot="item-text">首页</span>
       </tab-bar-item>
-      <tab-bar-item path="/category">
+      <tab-bar-item path="/category" activeColor="#ff5777">
         <img slot="item-img" src="~assets/img/tabbar/category.svg" alt=""/>
         <img slot="item-img-active" src="~assets/img/tabbar/category_active.svg" alt=""/>
         <span slot="item-text">分类</span>
       </tab-bar-item>
-      <tab-bar-item path="/shopcart">
+      <tab-bar-item path="/shopcart" activeColor="#ff5777">
         <img slot="item-img" src="~assets/img/tabbar/shopcart.svg" alt=""/>
         <img slot="item-img-active" src="~assets/img/tabbar/shopcart_active.svg" alt=""/>
         <span slot="item-text">购物车</span>
       </tab-bar-item>
-      <tab-bar-item path="/profile">
+      <tab-bar-item path="/profile" activeColor="#ff5777">
         <img slot="item-img" src="~assets/img/tabbar/profile.svg" alt=""/>
         <img slot="item-img-active" src="~assets/img/tabbar/profile_active.svg" alt=""/>
         <span slot="item-text">我的</span>
