ec87254b8f6ac9fbb4078a458f648468e7411b33
mr-hanwy <hanwy@vip.qq.com>
2022-05-11 23:28:04 +0800

    响应拦截器返回值优化

--- src/network/request.js
+++ src/network/request.js
@@ -19,7 +19,7 @@ export function request(config) {
   // 响应拦截
   instance.interceptors.response.use(result => {
     // 响应成功
-    return result;
+    return result.data;
   }, error => {
     // 服务器响应错误
     console.log(error);
