6783e70e3a18a5ee5d4c3002aaf5d89eb2754131
mr-hanwy <hanwy@vip.qq.com>
2022-05-11 23:37:27 +0800

    实现顶部标题栏

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,26 @@
 <template>
-  <h2>首页</h2>
+  <div>
+    <nav-bar class="nav-bar">
+      <div slot="nav-bar-center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/commons/navBar/NavBar";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
+.nav-bar {
+  text-align: center;
+  color: #fff;
+  background-color: var(--color-tint);
+}
 </style>
