c9c9a2e2f425649467096ee381f2ba0439d04b68
mr-hanwy <hanwy@vip.qq.com>
2022-05-11 23:13:33 +0800

    初始化网络请求组件

---
+++ src/network/request.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export function request(config) {
+  // 初始化配置
+  let instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000',
+    timeout: 5000
+  });
+
+  // 请求拦截
+  instance.interceptors.request.use(config => {
+    // 请求前操作
+    return config;
+  }, error => {
+    // 请求发送失败
+    console.log(error);
+  });
+
+  // 响应拦截
+  instance.interceptors.response.use(result => {
+    // 响应成功
+    return result;
+  }, error => {
+    // 服务器响应错误
+    console.log(error);
+  });
+
+  return instance(config);
+}
