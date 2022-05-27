cf2a8bf03814d2df66bac977941e71eb15788fa3
mr-hanwy <hanwy@vip.qq.com>
2022-05-27 20:41:16 +0800

    使用 fastclick 解决移动端点击事件 300ms 的延迟

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from "@/router";
 import store from "@/store";
+import Fastclick from 'fastclick';
 import Toast from "@/components/commons/toast";
 
-Vue.use(Toast);
+Vue.use(Toast)
 
 Vue.config.productionTip = false
+
+Fastclick.attach(document.body);
 
 Vue.prototype.$eventBus = new Vue();
 
